f5f6cf51f2b1594b792d8ca117e3a907a5174384
SofiVell <sofivell7@gmail.com>
2025-10-21 16:24:53 +0300

    Add files via upload

---
+++ Lab4a/Lab4a/Program.cs
@@ -0,0 +1,39 @@
+﻿// Варіант 8
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        Console.InputEncoding = System.Text.Encoding.UTF8;
+
+        Console.WriteLine("Оберіть напрямок конвертації:");
+        Console.WriteLine("1 — UAH → USD");
+        Console.WriteLine("2 — USD → UAH");
+
+        int choice = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Введіть курс: ");
+        double rate = Convert.ToDouble(Console.ReadLine());
+
+        switch (choice)
+        {
+            case 1:
+                Console.Write("Введіть суму в гривнях (UAH): ");
+                double uah = Convert.ToDouble(Console.ReadLine());
+                double usdResult = uah / rate;
+                Console.WriteLine($"Результат: {usdResult:F2} USD");
+                break;
+
+            case 2:
+                Console.Write("Введіть суму в доларах (USD): ");
+                double usd = Convert.ToDouble(Console.ReadLine());
+                double uahResult = usd * rate;
+                Console.WriteLine($"Результат: {uahResult:F2} UAH");
+                break;
+
+            default:
+                Console.WriteLine("Неправильний вибір! Введіть 1 або 2.");
+                break;
+        }
+    }
+}
